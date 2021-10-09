c86d6e206e2763134ca8236331561aaad3e048e4
kaneky08s <78807012+Kanek08@users.noreply.github.com>
2021-10-09 15:50:19 -0600

    menu del juego
    es una base del menu luego se agregan las variables y todo eso

---
+++ menu.py
@@ -0,0 +1,24 @@
+print ("Equipo Pokemon")
+print("1- Combate")
+print("2- Objetos")
+print("3- Pokemon")
+print("4- Escapar")
+opcion = int(input("¿Qué deberia hacer {pokemon}? "))
+if opcion == 1:
+    print("ataque 1")
+    print("ataque 2")
+    print("ataque 4")
+    print("ataque 3")
+elif opcion == 2:
+    print("Mochila")
+    print("1-Objetos curativos")
+    print("2-Objetos de combate")
+    print("3-Pokebolas")
+elif opcion == 3:
+    print("pokemon")
+    print("Pokemon 1")
+    print("1-sacar")
+    print("2-stats")
+elif opcion == 4:
+    print("Ha escado del combate")    
+
